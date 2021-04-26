017eac90556216954d49a2d410246e375e2da3dc
Irina Iakovleva <work@monty.ml>
2021-04-26 23:52:51 +0300

    nuxt init

---
+++ ssr-blog/components/header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="header">
+    <a href="/">Test list</a>
+    <img src="/cart.svg">
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  /* .header {
+    position: fixed;
+    top: 0
+  } */
+
+</style>
